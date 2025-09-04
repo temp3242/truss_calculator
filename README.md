f2cc6a5c46613cdb4e758045c93e255352c3885b
Arthur Gaudêncio <arthur3242.go@gmail.com>
2025-09-04 14:49:28 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Truss Calculator
+Calculadora de forças de treliças usando a biblioteca anaStruct.
+
+## Descrição
+Este código calcula as forças de reação em apoios não livres e as forças internas de cada elemento (Componentes x,y e o módulo orientado (<b>"+"</b> => Tração, <b>"-"</b> => Compressão))
+
+## Setup
+
+ - ### Crie o ambiente virtual
+        python3 -m venv .venv
+ - ### Instale as dependencias
+        .venv/bin/pip3 install -r requeriments.txt
+
+## Rodando o programa
+    .venv/bin/python src/main.py
+
+## Membros
+- ### Arthur Gaudêncio Odebrecht Stern
+- ### Gabriel Araújo da Silva Domingos Ferreira
+- ### Francisco Manuel da Silva Gomes
+- ### Rawlyson Oliveira de Macedo
---
+++ src/main.py
@@ -0,0 +1,99 @@
+from anastruct import SystemElements, Vertex
+import matplotlib.pyplot as plt
+from anastruct.fem.plotter import element
+import math
+import os
+
+from pathlib import Path
+
+path = Path.cwd().parent
+Path("../out").mkdir(parents=True, exist_ok=True)
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+ss = SystemElements()
+
+
+with open(find("especificacoes.txt", path)) as f:
+
+    vertices = []
+    non_free_pins = []
+
+    line = f.readline().rstrip()
+    vertices_elements_num = list(map(int, line.split('; ')))
+
+    for _ in range(vertices_elements_num[0]):
+        params = f.readline().rstrip().split('; ')
+        vertices.append(Vertex(float(params[1]), float(params[2])))
+
+
+    for i in range(len(vertices)):
+        line = list(map(int, f.readline().rstrip().split('; ')))
+        for j in range(i + 1, len(vertices)):
+            if line[j] == 1:
+                ss.add_truss_element([vertices[i], vertices[j]])
+
+    for v in vertices:
+        line = list(map(float, f.readline().rstrip().split('; ')))
+        if line[0] != 0:
+            ss.point_load(ss.find_node_id(v), Fx=line[0])
+        if line[1] != 0:
+            ss.point_load(ss.find_node_id(v), Fy=line[1])
+
+    for v in vertices:
+        pin_type = f.readline().rstrip()
+        if pin_type == 'P':
+            ss.add_support_hinged(ss.find_node_id(v))
+            non_free_pins.append(ss.find_node_id(v))
+        elif pin_type == 'X':
+            ss.add_support_roll(ss.find_node_id(v), direction=1)
+            non_free_pins.append(ss.find_node_id(v))
+        elif pin_type == 'Y':
+            ss.add_support_roll(ss.find_node_id(v), direction=2)
+            non_free_pins.append(ss.find_node_id(v))
+
+    ss.solve(max_iter=10000)
+    ss.show_structure(show=False)
+    plt.title('Estrutura')
+    plt.savefig('../out/estrutura.png')
+
+
+    ss.show_reaction_force(show=False)
+    plt.title('Forças de Reação')
+    plt.savefig('../out/reacao.png')
+
+    ss.show_axial_force(show=False)
+    plt.title('Forças axiais')
+    plt.savefig('../out/forcas_axiais.png')
+
+    ss.show_displacement(show=False)
+    plt.title('Deslocamento')
+    plt.savefig('../out/deslocamento.png')
+
+    #for res in ss.get_element_results():
+    #    for i in res:
+    #        print(f'{i}: {res[i]}')
+    #    print("\n")
+
+    #print(ss.get_node_results_ss(0))
+
+    for v in non_free_pins:
+        results = ss.get_node_results_system(v)
+        print(f'{round(results['Fx'],1)+0}; {round(results['Fy'],1)+0}')
+
+    results = ss.get_element_results()
+
+    for e in results:
+        alpha = e['alpha']
+        N = -float(e['Nmin'])  # ou Nmax
+
+        Fx = N * math.cos(alpha)
+        Fy = N * math.sin(alpha)
+
+        print(f"{Fx:.1f}, {Fy:.1f}, {N:.1f}")
+
+
+    f.close()
